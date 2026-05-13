14dffe6adcd7aed8db719762053d39e8454deee9
arifulbfd <ariful.bfd@gmail.com>
2026-05-13 11:22:41 +0600

    scout search

--- app/Models/Society.php
+++ app/Models/Society.php
@@ -6,10 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Scout\Engines\Engine;
+use Laravel\Scout\Scout;
+use Laravel\Scout\Searchable;
 
 class Society extends Model
 {
     use SoftDeletes;
+    use Searchable;
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array<string, mixed>
+     */
+    public function toSearchableArray(): array
+    {
+        $array = $this->toArray();
+ 
+        return $array;
+    }
+
+
+    /**
+     * Get the engine used to index the model.
+     */
+    public function searchableUsing(): Engine
+    {
+        return Scout::engine('database');
+    }
 
     protected $fillable = [
         'name',
